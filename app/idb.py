26013d763a7ffef4f31aebd767b57c8576f5403a
Nick Kantor <nick.kantor@utexas.edu>
2017-03-20 16:45:38 -0500

    Updated the makefile and cleaned up the code.

--- app/idb.py
+++ app/idb.py
@@ -1,59 +1,72 @@
+#!/usr/bin/env python3
+
+# pylint: disable = bad-whitespace
+# pylint: disable = invalid-name
+# pylint: disable = missing-docstring
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
-models = {'planetoids': 'Planetoid Bodies', 'stars': 'Stars', 
-		'galaxies': 'Galaxies', 'satellites': 'Satellites'}
+models = {'planetoids': 'Planetoid Bodies', 'stars': 'Stars',
+          'galaxies': 'Galaxies', 'satellites': 'Satellites'}
 
-headers = {'planetoids': ["Name", "Diameter", "Images", 
-    "Location", "Gravity", "Sol", "Orbital Period", "Age", 
-    "Surface Temperatures", "Body it Orbits"],
-    'galaxies': ["Name", "Images", "Location", "Age", 
-    "Year of Discovery", "Type" ],
-    'satellites': ["Name", "Orbital Period", "Year Launched", 
-    "Year Decommissioned", "Type of Mission"],
-    'stars': ["Name", "Diameter", "Images", "Location", "Age", 
-    "Temperature", "Type"] }
+headers = {'planetoids': ["Name", "Diameter", "Images",
+                          "Location", "Gravity", "Sol", "Orbital Period", "Age",
+                          "Surface Temperatures", "Body it Orbits"],
+           'galaxies': ["Name", "Images", "Location", "Age",
+                        "Year of Discovery", "Type"],
+           'satellites': ["Name", "Orbital Period", "Year Launched",
+                          "Year Decommissioned", "Type of Mission"],
+           'stars': ["Name", "Diameter", "Images", "Location", "Age",
+                     "Temperature", "Type"]}
+
 
 @app.route("/")
 def home():
     return render_template('home.html',
-    					   title='spacecowboys')
+                           title='spacecowboys')
+
 
 @app.route('/planetoids')
 def planetoid_table():
     return render_template('table.html',
                            title='planetoids',
                            model=models['planetoids'],
-                           headers= headers['planetoids'],
-                           rows= headers['planetoids'] )
+                           headers=headers['planetoids'],
+                           rows=headers['planetoids'])
+
 
 @app.route('/galaxies')
 def galaxies_table():
     return render_template('table.html',
                            title='galaxies',
                            model=models['galaxies'],
-                           headers= headers['galaxies'],
-                           rows= headers['galaxies'] )
+                           headers=headers['galaxies'],
+                           rows=headers['galaxies'])
+
 
 @app.route('/satellites')
 def satellites_table():
     return render_template('table.html',
                            title='satellites',
                            model=models['satellites'],
-                           headers= headers['satellites'],
-                           rows= headers['satellites'] )
+                           headers=headers['satellites'],
+                           rows=headers['satellites'])
+
 
 @app.route('/stars')
 def stars_table():
     return render_template('table.html',
                            title='stars',
                            model=models['stars'],
-                           headers= headers['stars'],
-                           rows= headers['stars'] )
+                           headers=headers['stars'],
+                           rows=headers['stars'])
+
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
 
 if __name__ == "__main__":
     app.run()
---
+++ app/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# pylint: disable = bad-whitespace
+# pylint: disable = invalid-name
+# pylint: disable = missing-docstring
