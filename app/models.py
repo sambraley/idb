46dba32211bf3eda9b3451317a74c1d36992ee47
Scott Farrior <farrior@cs.utexas.edu>
2017-03-21 17:11:24 -0500

    Adds the model for galaxies

--- app/models.py
+++ app/models.py
@@ -92,3 +92,49 @@ class Star(db.Model):
             "\nMass: " + self.mass +                       \
             "\nDistance: " + self.distance
 
+class Galaxy(db.Model):
+    # Attributes
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), unique = True)
+    images = db.Column(db.String())
+    right_ascension = db.Column(db.Float)
+    declination = db.Column(db.Float)
+    galaxy_type = db.Column(db.String())
+    redshift = db.Column(db.Float)
+    angular_size = db.Column(db.Float)
+
+    # Relations
+    stars = db.relationship('Star', backref = 'galaxy', lazy = 'dynamic')
+    planetoid_bodies = db.relationship('PlanetoidBody', backref = 'galaxy', lazy = 'dynamic')
+    satellites = db.relationship('Satellite', backref = 'galaxy', lazy = 'dynamic')
+    
+    # Methods
+    def __init__(self, name, images, right_ascension, declination, galaxy_type, redshift, 
+            angular_size, stars = None, planetoid_bodies = None, satellites = None):
+        # Check types
+        assert type(name) is str
+        assert type(images) is str
+        assert type(right_ascension) is float
+        assert type(declination) is float
+        assert type(galaxy_type) is str
+        assert type(redshift) is float
+        assert type(angular_size) is float
+
+        self.name = name
+        self.images = images
+        self.right_ascension = right_ascension
+        self.declination = declination
+        self.galaxy_type = galaxy_type
+        self.redshift = redshift
+        self.angular_size = angular_size
+        self.stars = stars
+        self.planetoid_bodies = planetoid_bodies
+        self.satellites = satellites
+
+    def __str__(self):
+        return "Name: " + self.name +                      \
+            "\nRight Ascension: " + self.right_ascension + \
+            "\nDeclination: " + self.declination +         \
+            "\nGalaxy Type: " + self.galaxy_type +         \
+            "\nRedshift: " + self.redshift +               \
+            "\nAngular Size: " + self.angular_size         
