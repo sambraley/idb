756007b48eaac3971d472deca3c86cb1f98a5761
dares23 <davidares7@gmail.com>
2017-04-03 13:49:50 -0500

    added components

---
+++ app/static/jsx/components/model_list_item.js
@@ -0,0 +1,15 @@
+class ModelListItem extends React.Component { 
+	render() {
+		return (
+				<div className="col-md-4 text-center service-box">
+      				<a href="">
+        			<img className="img-thumbnail about-image" src="/static/images/HAT-P-33.png" />
+        			<h3>Name</h3>
+      				</a>
+    			</div> 
+			);
+	}
+}
+
+
+export default ModelListItem;
