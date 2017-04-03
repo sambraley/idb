abbca0a8fdda8fc58b5ee6b9ca4a52404c839433
Nick Kantor <nick.kantor@utexas.edu>
2017-04-03 17:58:22 -0500

    Finished updating the carousel.

--- app/static/jsx/carouselItem.jsx
+++ app/static/jsx/carouselItem.jsx
@@ -3,9 +3,6 @@ class CarouselItem extends React.Component {
         return (
 					    <div className={"item " + this.props.class}>
 					    	<div className={"carousel-image"} style={{backgroundImage: 'url(' + this.props.url + ')'}}></div>
-					    	<div className={"carousel-content center"}>
-					        <h1>{this.props.message}</h1>
-					      </div>
 					    </div>
 				);
     }
