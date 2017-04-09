e7240eb9e4b99a2b8cf8d9b203f7b9763edc4f47
tabenmalik <tabenjmalik@gmail.com>
2017-04-09 15:45:35 -0500

    Fixed off-by-one error

--- nasa_scripts/data/star_imgs/star_compiler.py
+++ nasa_scripts/data/star_imgs/star_compiler.py
@@ -6,7 +6,7 @@ import json
 compiled = open("compiled_stars_imgs.json", "w")
 compiled_json = []
 
-for i in range(0, 270):
+for i in range(0, 271):
 		img_file = open("object_" + str(i) + "_img.json")
 		img_json = json.load(img_file)
 		img_dict = {"pid":i, "url":str(img_json["url"])}
