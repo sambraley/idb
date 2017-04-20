48473c5e45bfa23fe3741c72cd94197a210356c1
Taben Malik <tabenjmalik@gmail.com>
2017-04-20 14:49:19 -0500

    Puts markers ontop of geo map

--- app/static/js/geo_map.js
+++ app/static/js/geo_map.js
@@ -38,30 +38,29 @@ pull_concerts(1);
 // load and display the World
 function processData() {
 	d3.json("/static/csv/world-110m2.json", function(error, topology) {
-    g.selectAll("circle")
-       .data(all_concerts)
-       .enter()
-       .append("a")
-				  .attr("xlink:href", function(d) {
-					  return "https://www.google.com/search?q="+d.venue.city;}
-				  )
-       .append("circle")
-       .attr("cx", function(d) {
-               return projection([d.venue.longitude, d.venue.latitude])[0];
-       })
-       .attr("cy", function(d) {
-               return projection([d.venue.longitude, d.venue.latitude])[1];
-       })
-       .attr("r", 5)
-       .style("fill", "red");
-
-
-	g.selectAll("path")
-	      .data(topojson.object(topology, topology.objects.countries)
-	          .geometries)
-	    .enter()
-	      .append("path")
-	      .attr("d", path)
+        g.selectAll("path")
+              .data(topojson.object(topology, topology.objects.countries)
+                  .geometries)
+            .enter()
+              .append("path")
+              .attr("d", path);
+        
+        g.selectAll("circle")
+           .data(all_concerts)
+           .enter()
+           .append("a")
+                      .attr("xlink:href", function(d) {
+                          return "https://www.google.com/search?q="+d.venue.city;}
+                      )
+           .append("circle")
+           .attr("cx", function(d) {
+                   return projection([d.venue.longitude, d.venue.latitude])[0];
+           })
+           .attr("cy", function(d) {
+                   return projection([d.venue.longitude, d.venue.latitude])[1];
+           })
+           .attr("r", 5)
+           .style("fill", "red");
 	});
 }
 
@@ -70,9 +69,9 @@ var zoom = d3.behavior.zoom()
     .on("zoom",function() {
         g.attr("transform","translate("+ 
             d3.event.translate.join(",")+")scale("+d3.event.scale+")");
-        g.selectAll("circle")
-            .attr("d", path.projection(projection));
         g.selectAll("path")  
+            .attr("d", path.projection(projection));
+        g.selectAll("circle")
             .attr("d", path.projection(projection)); 
 
   });
